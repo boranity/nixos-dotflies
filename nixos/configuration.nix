7a618633ca8aac8091294a5686a015b84d24f2c4
Boranity <107348321+boranityy@users.noreply.github.com>
2022-08-11 18:00:27 +0300

    Update configuration.nix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -106,7 +106,13 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [ 
-  tdesktop   
+  tdesktop
+  cargo
+  gnumake
+  rustc
+  git
+  gcc
+  rustup
   ];
 
 
